63dc342a71780647583e6c12b1174f5ea1c5723a
Mariamhatem2 <mariam2206096@miuegypt.edu.eg>
2024-08-03 21:06:08 +0300

    Add files via upload

---
+++ chooseuser/scripts.js
@@ -0,0 +1,7 @@
+document.getElementById('AdminBtn').addEventListener('click', function() {
+    alert('Admin button clicked');
+});
+
+document.getElementById('userBtn').addEventListener('click', function() {
+    alert('user clicked');
+});
