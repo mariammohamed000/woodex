78d47b394272e925de11adadded9cb45f74622c0
mariammohamed000 <156384424+mariammohamed000@users.noreply.github.com>
2024-08-10 20:04:07 +0300

    Delete sign directory

--- sign/scripts.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('signUpBtn').addEventListener('click', function() {
-    alert('Sign Up button clicked');
-});
-
-document.getElementById('signInBtn').addEventListener('click', function() {
-    alert('Sign In button clicked');
-});
