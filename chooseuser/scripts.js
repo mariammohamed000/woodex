57eb48e8a02f7104169d777e8ca5b587765e1594
mariammohamed000 <156384424+mariammohamed000@users.noreply.github.com>
2024-08-10 20:05:14 +0300

    Delete chooseuser directory

--- chooseuser/scripts.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('AdminBtn').addEventListener('click', function() {
-    alert('Admin button clicked');
-});
-
-document.getElementById('userBtn').addEventListener('click', function() {
-    alert('user clicked');
-});
